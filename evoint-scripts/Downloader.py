8aa554f22cbd4297c6ddbe0db77e5ddc2f8c17d2
Schild, Paul <Paul.Schild@mdc-berlin.de>
2021-09-21 17:41:57 +0200

    downoad metadata for all years available, not just given years

--- evoint-scripts/Downloader.py
+++ evoint-scripts/Downloader.py
@@ -52,22 +52,43 @@ def download(pub):
     # os.chdir("..")
 
 
+def is_link_to_volume(link: str):
+    last = link.split("/")[-1]
+    return last[:4].isdigit()
+
+
+def get_available_volumes(index_page_url=None):
+    result = []
+    if index_page_url is None:
+        index_page_url = "https://www.ijcai.org/past_proceedings/"
+    response = urlopen(index_page_url)
+    page_source = response.read()
+    soup = BeautifulSoup(page_source, 'html.parser')
+    for link in soup.find_all('a'):
+        current_link = link.get('href')
+        if is_link_to_volume(current_link):
+            if index_page_url not in current_link:
+                current_link = urllib.parse.urljoin(index_page_url, current_link)
+            result.append(current_link)
+    return result.reverse()
+
+
 # Single Volume: 1969 - 1975 & 2003 - ...
-def download_from_single_volume_years(desired_years=None):
-    if desired_years is None:
-        desired_years = list(range(1969, 1977, 2)) + list(range(2003, 2017, 2)) + list(range(2016, 2021))
+def download_from_single_volume_years():
 
     base_pdf_path = '../vikus-viewer/data/fulltext/pdf/'
     base_url = "https://www.ijcai.org/Proceedings/"
 
-    for desired_year in desired_years:
-        desired_year = str(desired_year)
-        os.makedirs(base_pdf_path + desired_year, exist_ok=True)
-        year = years[desired_year] if desired_year in years.keys() else Year(desired_year)
+    available_volumes = get_available_volumes(base_url)
+
+    for volume_link in available_volumes:
+        year = volume_link.split("/")[-1][:4]
+        os.makedirs(base_pdf_path + year, exist_ok=True)
+        year_instance = years[year] if year in years.keys() else Year(year)
 
         file_links = []
-        year_url = urllib.parse.urljoin(base_url, str(year.year))
-        response = urlopen(year_url)  # opens the URL
+        year_url = urllib.parse.urljoin(base_url, str(year_instance.year))
+        response = urlopen(volume_link)  # opens the URL
         page_source = response.read()
         soup = BeautifulSoup(page_source, 'html.parser')
         for link in soup.find_all('a'):
@@ -82,16 +103,16 @@ def download_from_single_volume_years(desired_years=None):
 
                 pub_id = next_pub_id()
                 title = parse_title(link.text)
-                path_to_pdf = base_pdf_path + str(year.year) + '/' + str(pub_id) + '.pdf'
-                print(f'{year.year}:')
+                path_to_pdf = base_pdf_path + str(year_instance.year) + '/' + str(pub_id) + '.pdf'
+                print(f'{year_instance.year}:')
                 print(f'Creating Publication_Object "{title}"')
                 publication = Publication(pub_id=pub_id,
                                           title=title,
-                                          year=year.year,
+                                          year=year_instance.year,
                                           origin_path=current_link,
                                           path_to_pdf=path_to_pdf)
 
-                download(publication)
+                # download(publication)
 
                 # TODO: new Method 'add_to_data_csv(publication)'
                 create_data_csv(publications)
@@ -111,16 +132,16 @@ def create_vikus_textures_and_sprites():
     os.system("node ../vikus-viewer-script/bin/textures 'data/thumbnails/*.png' --output '../vikus-viewer/data/images'")
 
 
-# download_from_single_volume_years()
+download_from_single_volume_years()
 
-read_data_csv()
+# read_data_csv()
 # create_data_csv(publications)
 # pdf_to_thumbnail(publications)
 # create_vikus_textures_and_sprites()
 
 # get_keywords_from_csv()
-for publication in publications.values():
-    print(publication.id)
-    fulltext = publication.fulltext()
-    publication.set_keywords(fulltext)
-create_data_csv(publications)
+# for publication in publications.values():
+#     print(publication.id)
+#     fulltext = publication.fulltext()
+#     publication.set_keywords(fulltext)
+# create_data_csv(publications)
